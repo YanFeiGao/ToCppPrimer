1376f7963651b22daecb68916c4260b612c7a060
YanFeiGao <mwnu18394518721@gmail.com>
2017-09-19 23:34:29 +0800

    序言章节 p0-p11

---
+++ CppPrimer_Practice/CppPrimer_Practice/CppPrimerPractice.cpp
@@ -0,0 +1,10 @@
+// CppPrimerPractice.cpp: 定义控制台应用程序的入口点。
+//
+#include "stdafx.h"
+#include <iostream>
+int main()
+{
+
+	return 0;
+}
+
---
+++ CppPrimer_Practice/exercise1_3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main()
+{
+	//endl结束当前行，将与设备关联的缓冲区中的内容刷新到设备中。
+	std::cout << "Hello,World" << std::endl;
+	return 0;
+}
